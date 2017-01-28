8c783972f816b43b41f4700e15b0f7a0aceaee04
chriswaddell <chriswaddell.waddell32@gmail.com>
2017-01-27 22:21:04 -0500

    Test again

--- Frame.py
+++ Frame.py
@@ -9,6 +9,7 @@ import matplotlib.cm as cm
 #TODO: Use better approximation for the spectrum
 #TODO: Implement approximation to spin-weighted spherical harmonics
 #TODO: Add instrumental noise
+#TODO: Make code great again
 
 uK = const.uK
 
